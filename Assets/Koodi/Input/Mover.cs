73b3daa5bdeaa8e99f6b79cb1376951907e0c62b
Antti Helin <antti.helin@tuni.fi>
2023-03-06 16:31:57 +0200

    Swaippaus jotenkin toimimaan

--- Assets/Koodi/Input/Mover.cs
+++ Assets/Koodi/Input/Mover.cs
@@ -6,25 +6,120 @@ namespace Autopeli
 {
     public class Mover : MonoBehaviour
     {
+        // Define lanes
         [SerializeField]
-        private float speed = 1;
+        private float[] lanePositions = new float[] { -1.06f, -2.81f, -4.31f };
 
-        private Vector2 direction;
-        private InputReader inputReader;
+        // Starts at the middle
+        private int currentLane = 1;
 
+        private Vector2 startTouchPosition;
+        private Vector2 endTouchPosition;
+        private Vector2 currentPosition;
+        private bool stopTouch = false;
 
+        // how long of a swipe to detect 
+        [SerializeField]
+        private float swipeRange;
+        [SerializeField]
+        private float tapRange;
+
+        // the duration of the lane change animation
+        [SerializeField]
+        private float laneChangeDuration;
+
+        // Starts at the middle put in use
         private void Awake()
         {
-            inputReader = GetComponent<InputReader>();
+            transform.position = new Vector3(transform.position.x, lanePositions[currentLane]);
         }
 
-
-        private void Update()
+        void Update()
         {
-            this.direction = inputReader.GetMoveInput();
-            Vector2 movement = this.speed * Time.fixedDeltaTime * direction;
+            Swipe();
+        }
 
-            transform.Translate(movement);
+        public void Swipe()
+        {
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                startTouchPosition = Input.GetTouch(0).position;
+            }
+
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                currentPosition = Input.GetTouch(0).position;
+                Vector2 Distance = currentPosition - startTouchPosition;
+
+                if (!stopTouch)
+                {
+                    // Move up if not at the 0 lane
+                    if (Distance.y > swipeRange)
+                    {
+                        Debug.Log("Up");
+                        if (currentLane > 0)
+                        {
+                            currentLane--;
+                            StartCoroutine(MoveToLane(lanePositions[currentLane]));
+                        }
+                        else if (currentLane <= 0)
+                        {
+                            currentLane = 0;
+                            StartCoroutine(MoveToLane(lanePositions[currentLane]));
+                        }
+
+                        stopTouch = true;
+
+                    }
+                    // Move down if not at the 2 lane
+                    else if (Distance.y < -swipeRange)
+                    {
+                        Debug.Log("Down");
+                        if (currentLane < 2)
+                        {
+                            currentLane++;
+                            StartCoroutine(MoveToLane(lanePositions[currentLane]));
+                        }
+                        else if (currentLane >= 2)
+                        {
+                            currentLane = 2;
+                            StartCoroutine(MoveToLane(lanePositions[currentLane]));
+                        }
+
+                        stopTouch = true;
+                    }
+                }
+            }
+
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                stopTouch = false;
+
+                endTouchPosition = Input.GetTouch(0).position;
+
+                Vector2 Distance = endTouchPosition - startTouchPosition;
+
+                // Tap 
+                if (Mathf.Abs(Distance.x) < tapRange && Mathf.Abs(Distance.y) < tapRange)
+                {
+                    Debug.Log("Tap");
+                }
+            }
+        }
+
+        private IEnumerator MoveToLane(float targetPositionY)
+        {
+            float startTime = Time.time;
+            float startPositionY = transform.position.y;
+
+            while (Time.time < startTime + laneChangeDuration)
+            {
+                float t = (Time.time - startTime) / laneChangeDuration;
+                transform.position = new Vector3(transform.position.x, Mathf.Lerp(startPositionY, targetPositionY, t));
+                yield return null;
+            }
+
+            transform.position = new Vector3(transform.position.x, targetPositionY);
         }
     }
 }
