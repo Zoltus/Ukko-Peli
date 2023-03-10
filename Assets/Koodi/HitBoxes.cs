fd1cc4ec969334806c8c7664d28f3db7ed137daf
Antti Helin <antti.helin@tuni.fi>
2023-03-10 10:55:48 +0200

    Pop-up yritys

--- Assets/Koodi/HitBoxes.cs
+++ Assets/Koodi/HitBoxes.cs
@@ -6,13 +6,25 @@ namespace Autopeli
 {
     public class HitBoxes : MonoBehaviour
     {
+        [SerializeField]
+        private GameObject kysymys;
+
+
+
+        private void Awake()
+        {
+    
+            
+            kysymys.SetActive(false);
+        }
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
             Destroy(this.gameObject);
+            kysymys.SetActive(true);
             
         }
 
-       
+        
     }
 }
