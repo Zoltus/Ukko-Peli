db41b65796d77ca1519c33cf6f282db503764965
Antti Helin <antti.helin@tuni.fi>
2023-03-13 16:41:57 +0200

    Toinen proto

--- Assets/Koodi/HitBoxes.cs
+++ Assets/Koodi/HitBoxes.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Unity.VisualScripting.Member;
 
 namespace Autopeli
 {
@@ -8,16 +9,19 @@ namespace Autopeli
     {
         [SerializeField]
         private GameObject kysymys;
-
-
+        
+        private static AudioSource source;
 
         private void Awake()
         {
+            source = GetComponent<AudioSource>();
             kysymys.SetActive(false);
         }
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
+            
+            source.Play();
             Destroy(this.gameObject);
             kysymys.SetActive(true);
             TaustanLiikuttaminen.SlowDown();
