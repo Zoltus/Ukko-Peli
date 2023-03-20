02ffa69ad9e7ec6ca8c6f92b0b4ca45b9f59d484
Antti Helin <antti.helin@tuni.fi>
2023-03-20 20:16:20 +0200

    a

--- Assets/Koodi/KysymysValinta/Menut/Continue.cs
+++ Assets/Koodi/KysymysValinta/Menut/Continue.cs
@@ -4,30 +4,17 @@ using UnityEngine;
 
 namespace Autopeli
 {
-    public class Pausing : MonoBehaviour
+    public class Continuing : MonoBehaviour
     {
         [SerializeField]
         public GameObject camera;
 
         [SerializeField]
         public GameObject menuToOpen;
-
-        [SerializeField]
-        public GameObject PauseButton;
-
-        private void Awake()
-        {
-            camera.GetComponent<AudioListener>().enabled = false;
-            //text.SetActive(true);
-            menuToOpen.SetActive(true);
-            Time.timeScale = 0;
-        }
-
         public void PauseGame()
         {
             if (Time.timeScale > 0)
             {
-                PauseButton.SetActive(false);
                 camera.GetComponent<AudioListener>().enabled = false;
                 //text.SetActive(true);
                 menuToOpen.SetActive(true);
@@ -35,7 +22,6 @@ namespace Autopeli
             }
             else
             {
-                PauseButton.SetActive(true);
                 camera.GetComponent<AudioListener>().enabled = true;
                 menuToOpen.SetActive(false);
                 //text.SetActive(false);
