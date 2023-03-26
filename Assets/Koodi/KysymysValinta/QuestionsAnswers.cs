0fe45d3b0bfd3368938ce6c5f348f8f8b1404a3e
Zoltus <zoltus@outlook.com>
2023-03-26 18:11:37 +0300

    t

---
+++ Assets/Koodi/KysymysValinta/QuestionsAnswers.cs
@@ -0,0 +1,13 @@
+
+using System;
+
+namespace Autopeli
+{
+    [Serializable]
+    public class QuestionsAnswers
+    {
+        public string Question;
+        public string[] Answers;
+        public int CorrectAnswer;
+    }
+}
