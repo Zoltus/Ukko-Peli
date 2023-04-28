33531df43ecaa55451bd0f28e0806f987f5f14fb
Zoltus <zoltus@outlook.com>
2023-04-28 17:06:10 +0300

    fixed score

---
+++ Assets/ResetScore.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Autopeli {
+    public class ResetScore : MonoBehaviour {
+        // Start is called before the first frame update
+        void Start() {
+            GameManager.start();
+        }
+
+        // Update is called once per frame
+        void Update() { }
+    }
+}
