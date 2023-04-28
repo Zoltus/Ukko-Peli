b8523f5aff19cb02687a85236ca54e0bb497d3c1
Zoltus <zoltus@outlook.com>
2023-04-28 14:02:13 +0300

    flag shadows when selecting languages

--- Assets/Koodi/EnglishButton.cs
+++ Assets/Koodi/EnglishButton.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Autopeli
 {
     public class EnglishButton : MonoBehaviour {
+        [SerializeField] GameObject fiButton;
         public void changeLanguage() {
             Debug.Log("Changed to eng");
             LanguageManager.SetLanguage(1);
+
+            SuomiButton.handleShadows();
         }
     }
 }
