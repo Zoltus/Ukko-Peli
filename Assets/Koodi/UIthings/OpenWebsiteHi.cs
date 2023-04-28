40fef4699ead306b2412cf42edd140d41bbe1be8
Antti Helin <antti.helin@tuni.fi>
2023-04-28 15:17:08 +0300

    Hiscore

---
+++ Assets/Koodi/UIthings/OpenWebsiteHi.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Autopeli
+{
+    public class OpenWebsiteHi : MonoBehaviour
+    {
+        public void OpenLinkWebsite()
+        {
+            Application.OpenURL("https://miika-veeti-highscore1.onrender.com/scores/html");
+        }
+
+
+    }
+}
